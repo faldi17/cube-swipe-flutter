f0841918ec301b6f21eb29c0bb5c9b5f76e36e99
Faldi <faldiansyahk@gmail.com>
2025-11-23 18:45:32 +0700

    fixed cube wipe

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,19 +12,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepOrange[200],
+      backgroundColor: Colors.lightBlue[200],
       body: CarouselSlider(
         slideTransform: CubeTransform(),
         slideIndicator: CircularSlideIndicator(
-          padding: EdgeInsets.only(bottom: 20),
+          padding: EdgeInsets.only(bottom: 50),
+          currentIndicatorColor: Colors.white,
         ),
         unlimitedMode: true,
         children: [
           Container(
-            color: Colors.deepOrange,
+            color: Colors.lightBlue,
           ),
           Container(
-            color: Colors.lightBlue,
+            color: Colors.lightBlue.shade400,
+          ),
+          Container(
+            color: Colors.lightBlue.shade300,
           ),
         ],
       ),
